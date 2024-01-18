e677679a6f32d44ab9781a8d8491634ccc291da1
Auseles <auseles15@mail.ru>
2024-01-19 02:19:48 +0300

    1.0

---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,56 @@
+using Newtonsoft.Json;
+using System.Net.Http.Json;
+using System.Threading;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public class LondonTime
+        {
+            public string abbreviation;
+            public string datetime;
+            public int day_of_week;
+            public int day_of_year;
+            public bool dst;
+            public string dst_from;
+            public int dst_offset;
+            public string dst_until;
+            public int raw_offset;
+            public string timezone;
+            public int unixtime;
+            public string utc_datetime;
+            public string utc_offset;
+            public int week_number;
+        }
+        public async void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 1; i < 11; i++)
+            {
+                Thread myThread = new(LoadJson);
+                myThread.Start();
+                await Task.Delay(1000);
+            }
+        }
+        private static HttpClient sharedClient = new()
+        {
+            BaseAddress = new Uri("http://worldtimeapi.org/api/timezone/Europe/London"),
+        };
+        public async void LoadJson()
+        {
+            var rest = await sharedClient.GetAsync(sharedClient.BaseAddress);
+            string json = rest.Content.ReadAsStringAsync().Result;
+            LondonTime deserialized = JsonConvert.DeserializeObject<LondonTime>(json);
+            label1.Invoke((Action)delegate { label1.Text = deserialized.datetime; });
+            Thread.Sleep(5000);
+            LoadJson();
+        }
+    }
+}
