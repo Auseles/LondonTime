cd6ddc48f51e7e7e587b6ccd33fc2c7cde08d86c
Auseles <auseles15@mail.ru>
2024-01-19 11:39:50 +0300

    3

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             label1 = new Label();
+            label2 = new Label();
             SuspendLayout();
             // 
             // label1
@@ -40,11 +41,21 @@
             label1.TabIndex = 0;
             label1.Text = "label1";
             // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(28, 43);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 15);
+            label2.TabIndex = 1;
+            label2.Text = "label2";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(221, 37);
+            ClientSize = new Size(221, 76);
+            Controls.Add(label2);
             Controls.Add(label1);
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
@@ -57,5 +68,6 @@
         #endregion
 
         private Label label1;
+        private Label label2;
     }
 }
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -4,6 +4,7 @@ using System.Threading;
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace WinFormsApp1
 {
@@ -30,28 +31,78 @@ namespace WinFormsApp1
             public string utc_offset;
             public int week_number;
         }
-        public async void Form1_Load(object sender, EventArgs e)
+
+        public void Form1_Load(object sender, EventArgs e)
         {
-            for (int i = 1; i < 11; i++)
+            Start();
+        }
+        bool flag = true;
+        int cntRequest = 0;
+        public async void Start()
+        {
+            flag = true;
+            int delay = CheckTime();
+            while (flag)
             {
-                Thread myThread = new(LoadJson);
-                myThread.Start();
-                await Task.Delay(500);
+                //int delay = CheckTime();
+                for (int i = 1; i < 11; i++)
+                {
+                    Thread myThread = new(LoadJson);
+                    myThread.Start();
+                    await Task.Delay(delay);
+                }
             }
+        }
+        public int CheckTime()
+        {
+            var datatime = DateTime.Now.ToString("ss,ffffff");
+            double time = double.Parse(datatime);
+            double diffTime = 60 - time;
+            double delay = Math.Floor(diffTime / (125 - cntRequest) * 1000);
+            return Convert.ToInt32(delay);
         }
         private static HttpClient sharedClient = new()
         {
             BaseAddress = new Uri("http://worldtimeapi.org/api/timezone/Europe/London"),
         };
+        object locker = new();
         public async void LoadJson()
         {
-            var rest = await sharedClient.GetAsync(sharedClient.BaseAddress);
-            string json = rest.Content.ReadAsStringAsync().Result;
-            LondonTime deserialized = JsonConvert.DeserializeObject<LondonTime>(json);
-            if (label1.Text != deserialized.datetime)
-            label1.Invoke((Action)delegate { label1.Text = deserialized.datetime; });
-            Thread.Sleep(5000);
-            LoadJson();
+            if (cntRequest < 120)
+            {
+                try
+                {
+                    lock (locker)
+                    {
+                        var rest = sharedClient.GetAsync(sharedClient.BaseAddress).Result;
+                        string json = rest.Content.ReadAsStringAsync().Result;
+                        LondonTime deserialized = JsonConvert.DeserializeObject<LondonTime>(json);
+                        if (label1.Text != deserialized.datetime)
+                        {
+                            label1.Invoke((Action)delegate { label1.Text = deserialized.datetime; });
+                            label2.Invoke((Action)delegate { label2.Text = "Кол-во иттераций за минуту: " + cntRequest++.ToString(); });
+                        }
+                    }
+                    //int delay = CheckTime();
+                    //await Task.Delay(delay);
+                }
+                catch (HttpRequestException ex)
+                {
+                    label1.Invoke((Action)delegate { "Ошибка соединения".ToString(); });
+                }
+            }          
+            else { cntRequest = 0; }
+            //else 
+            //{
+            //    var datatime = DateTime.Now.ToString("ss,ffffff");
+            //    double time = double.Parse(datatime);
+            //    if (time > 50)
+            //    {
+            //        await Task.Delay(Convert.ToInt32(Math.Ceiling((60 - time) * 1000)));
+            //    }
+            //    cntRequest = 0;
+            //}
         }
+
     }
 }
